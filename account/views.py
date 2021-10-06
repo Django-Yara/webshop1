b8d98c95a116e07bdae0cd432a5f5189bb6254cb
Student <student@lan.itstep.kiev.ua>
2021-10-06 19:24:28 +0300

    добавлены модели и форма регистрации

--- account/views.py
+++ account/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+# У каждого шаблона должен быть свой вид (контроллер)
+# Виды, Контроллеры. Каждый вид определяет сценарий загрузки шаблонов.
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'account/register.html', context={
+            'page_name': 'Регистрация',
+            'page_app': 'account',
+            'page_view': 'register'
+        })
+    elif request.method == 'POST':
+        login x = request.POST.get('login')
+        pass1 x = request.POST.get('pass1')
+        pass2 x = request.POST.get('pass2')
+        email x = request.POST.get('email')
 
-# Create your views here.
+        # 1 - Валидация данных ...
+
+        # 2 - Сохранение пользователя в Базе Данных
+
+        # 3 - Вывод отчёта ... Страница отчёта о регистрации
+        return render(request, 'account/report.html', context={
+            'page_name': 'Отчёт о регистрации',
+            'page_app': 'account',
+            'page_view': 'report'
+        })
+    # Нуджно дома: 1. создать страничку и 2. Прописать маршруты этой страницы
--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    # свойства моделей. Каждое св-во - это столбец в соотв таблице
+    title = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return str(self.title)
+
+
